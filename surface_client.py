28bf439da0bdd68c221fee35e3decddd22802e30
Andrew Berkley <ajberkley@gmail.com>
2019-10-14 16:33:27 -0700

    Add more features

--- surface_client.py
+++ surface_client.py
@@ -2,12 +2,26 @@
 import requests
 import csv
 import json
-URL='http://surface.canadarasp.com:8080/data'
+import argparse
+VERSION="0.1 OCTOBER 2019"
+AUTHOR="ajberkley@gmail.com"
 
-def get_data_at_point(lon, lat, start_time, end_time=None):
+# URL='http://surface.canadarasp.com:8080/data'
+URL='http://localhost:8080/data'
+
+parser = argparse.ArgumentParser(description='For a single longitude and latitude point, generate a CSV file from EC surface archive (%s %s) across a span of time' % (VERSION, AUTHOR))
+parser.add_argument('-lon', dest='lon', type=float, help='Longitude of point', required=True)
+parser.add_argument('-lat', dest='lat', type=float, help='Latitude of point', required=True)
+parser.add_argument('-start', dest='start_time', help='RFC3339 time stamp of starting date/time like 2019-10-01T07:00:00Z', required=True)
+parser.add_argument('-end', dest='end_time', help='RFC3339 time stamp of ending date/time like 2019-10-01T08:00:00Z')
+parser.add_argument('-var', dest='var', help='sfc_temp, sfc_pres, or wind')
+parser.add_argument('-output', help='Output CSV filename')
+
+def get_data_at_point(lon, lat, start_time, var, end_time=None):
     lon_lat_bbox = '%f, %f' % (lon, lat)
-    query = {'lon-lat-bbox': lon_lat_bbox, 'start-time': start_time, 'end-time': end_time or start_time}
+    query = {'lon-lat-bbox': lon_lat_bbox, 'start-time': start_time, 'end-time': end_time or start_time, 'var': var}
     r = requests.post(URL, data=query)
+    print(r.text)
     return json.loads(r.content)
 
 def write_dicts_to_csv(file, data):
@@ -18,4 +32,6 @@ def write_dicts_to_csv(file, data):
         for datum in data:
             writer.writerow(datum)
 
-write_dicts_to_csv('blarg.csv',get_data_at_point(-122.43, 48.0, '2019-10-01T07:00:00Z', '2019-10-01T08:00:00Z'))
+args = parser.parse_args()
+            
+write_dicts_to_csv('blarg.csv',get_data_at_point(args.lon, args.lat, args.start_time, args.var, args.end_time or None))
