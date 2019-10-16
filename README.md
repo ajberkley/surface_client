6d225ec34d5b5babd23ab9bc4e950d2a07817c0f
Andrew Berkley <ajb@dwavesys.com>
2019-10-15 17:29:39 -0700

    Update to support region requests

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # surface_client
-A Python client to interface with http://surface.canadarasp.com which serves archived HRDPS surface data.
+A Python client to interface with surface.canadarasp.com which serves archived HRDPS surface data.
+
+To install:
+git clone https://github.com/ajberkley/surface_client.git
+cd surface_client
+virtualenv -p `which python3.7` env
+source env/bin/activate
+pip install -r requirements.txt
+python surface_client.py -h
+python surface_client.py -lon -122.0 -lat 48.0 -start 2019-10-03T00:00:00Z -end 2019-10-03T01:00:00Z -var sfc_temp
+python surface_client.py -lon -122.0 -lat 48.0 -lon2 -122.01 -lat2 48.1 -start 2019-10-03T00:00:00Z -end 2019-10-03T01:00:00Z -var sfc_temp
+
--- surface_client.py
+++ surface_client.py
@@ -7,22 +7,32 @@ import sys
 VERSION="v0.1 October 2019"
 AUTHOR="ajberkley@gmail.com"
 
-PROG_DESCRIPTION='For a single longitude and latitude point, generate a CSV file from EC surface archive across a span of time -- %s %s' % (AUTHOR, VERSION)
+PROG_DESCRIPTION='For a single longitude and latitude point or a region, generate a CSV file from EC surface archive across a span of time -- %s %s' % (AUTHOR, VERSION)
 
 parser = argparse.ArgumentParser(description=PROG_DESCRIPTION)
 parser.add_argument('-lon', dest='lon', type=float, help='Longitude of point', required=True)
 parser.add_argument('-lat', dest='lat', type=float, help='Latitude of point', required=True)
+parser.add_argument('-lon2', dest='lon_e', type=float, help='Longitude of other corner if one wants a region')
+parser.add_argument('-lat2', dest='lat_e', type=float, help='Latitude of other corner if one wants a region')
 parser.add_argument('-start', dest='start_time', help='RFC3339 time stamp of starting date/time like 2019-10-01T07:00:00Z', required=True)
 parser.add_argument('-end', dest='end_time', help='RFC3339 time stamp of ending date/time like 2019-10-01T08:00:00Z')
 parser.add_argument('-var', dest='var', help='sfc_temp, sfc_pres, or wind')
 parser.add_argument('-output', dest='output', help='Output CSV filename')
 parser.add_argument('-url', dest='url', help='Default is http://surface.canadarasp.com:8080/data', default='http://surface.canadarasp.com:8080/data')
 
+def send_request(data):
+    r = requests.post(args.url, data=data)
+    return json.loads(r.content)
+
+def get_data_for_region(lona, lata, lonb, latb, start_time, var, end_time=None):
+    lon_lat_bbox = '%f, %f, %f, %f' % (lona, lata, lonb, latb)
+    query = {'lon-lat-bbox': lon_lat_bbox, 'start-time': start_time, 'end-time': end_time or start_time, 'var': var}
+    return send_request(query)
+
 def get_data_at_point(lon, lat, start_time, var, end_time=None):
     lon_lat_bbox = '%f, %f' % (lon, lat)
     query = {'lon-lat-bbox': lon_lat_bbox, 'start-time': start_time, 'end-time': end_time or start_time, 'var': var}
-    r = requests.post(args.url, data=query)
-    return json.loads(r.content)
+    return send_request(query)
 
 def write_dicts_to_csv(writeable, data):
     keys = data[0].keys()
@@ -32,8 +42,10 @@ def write_dicts_to_csv(writeable, data):
         writer.writerow(datum)
 
 args = parser.parse_args()
-
-data = get_data_at_point(args.lon, args.lat, args.start_time, args.var, args.end_time or None)
+if args.lon_e and args.lat_e:
+    data = get_data_for_region(args.lon, args.lat, args.lon_e, args.lat_e, args.start_time, args.var, args.end_time or None)
+else:
+    data = get_data_at_point(args.lon, args.lat, args.start_time, args.var, args.end_time or None)
 if args.output:
     with open(file,'w') as file:
         write_dicts_to_csv(file, data)
